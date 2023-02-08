755add100fa9e2d4acb5f2b1698615a8a3501739
mt01463 <sanghoon.baek@seeroo.co.kr>
2023-02-08 14:39:32 +0900

    add map

--- TestMapKit/ContentView.swift
+++ TestMapKit/ContentView.swift
@@ -6,14 +6,54 @@
 //
 
 import SwiftUI
+import MapKit
 
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView(frame: .zero)
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+
+    func updateUIView(_ view: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 37.5666791, longitude: 126.9782914)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        view.setRegion(region, animated: true)
+        view.isZoomEnabled = true
+        view.isPitchEnabled = true
+        view.isMultipleTouchEnabled = true
+        
+        let mapCamera = MKMapCamera()
+        mapCamera.centerCoordinate = coordinate
+        mapCamera.pitch = 85
+        mapCamera.altitude = 100
+        mapCamera.heading = 45
+        
+        view.camera = mapCamera
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate {
+        var parent: MapView
+
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+
+        func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+            // Add your custom code to color the selected building here
+        }
+    }
+}
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            MapView()
         }
         .padding()
     }
