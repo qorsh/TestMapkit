67a49a77541b4d8515ba484d5f394f77071c3b69
mt01463 <sanghoon.baek@seeroo.co.kr>
2023-02-08 11:59:38 +0900

    Initial Commit

---
+++ TestMapKit/TestMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestMapKitApp.swift
+//  TestMapKit
+//
+//  Created by kbcard on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct TestMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
